e9fd88eb865858095b23b06af273b582db0be439
Petro Hubskyi <petradix@gmail.com>
2019-06-02 12:02:52 +0300

    New output added to Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        System.out.println("Hello world");
+
+        System.out.println("Hello Git!");
     }
 }
